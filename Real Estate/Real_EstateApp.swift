ce91278b01577adea5bcc75f0cc1359b79154b92
Cheppar <ejioforched@gmail.com>
2023-08-13 20:27:44 +0300

    Initial Commit

---
+++ Real Estate/Real_EstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_EstateApp.swift
+//  Real Estate
+//
+//  Created by Mac User on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Real_EstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
